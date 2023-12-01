c4c973ebdc93e716d67b75759044db07830c308a
johnatilley <1987301+johnatilley@users.noreply.github.com>
2023-12-01 10:23:38 +0000

    Improved the server logic

--- api.js
+++ api.js
@@ -1,21 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require("path");
+const app = express();
 
-const port = 4000;
-
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/api/river_sensor_data', (req, res) => {
   const river_sensor_data = require('./river_sensor_data.json');
-  res.json(river_sensor_data)
+  res.json(river_sensor_data);
 });
 
+// Any api route that doesn't exist will return a not found
 app.get('/api/*', (req, res) => {
-  const river_sensor_data = require('./river_sensor_data.json');
-  res.send(`Path ${req.url} not found!`)
+  res.status(404).json({ error: { message: `Path ${req.url} not found!` } });
 });
 
-app.use(express.static("build"));
+// Every other route goes to react which will handle any 404s
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
